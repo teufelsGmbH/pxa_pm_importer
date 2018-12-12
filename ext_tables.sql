4fc3240db7f960b2cc7cfde6927de748226369fd
Mats Svensson <mats@pixelant.se>
2018-12-12 09:01:16 +0100

    [TASK] add indexes for default import hash field in tables

--- ext_tables.sql
+++ ext_tables.sql
@@ -27,7 +27,9 @@ CREATE TABLE tx_pxapmimporter_domain_model_import (
 #
 CREATE TABLE tx_pxaproductmanager_domain_model_product (
 	pm_importer_import_id_hash varchar(55) DEFAULT '' NOT NULL,
-	pm_importer_import_id varchar(55) DEFAULT '' NOT NULL
+	pm_importer_import_id varchar(55) DEFAULT '' NOT NULL,
+
+	KEY importhash (pm_importer_import_id_hash ,sys_language_uid, pid)
 );
 
 #
@@ -35,7 +37,9 @@ CREATE TABLE tx_pxaproductmanager_domain_model_product (
 #
 CREATE TABLE sys_category (
 	pm_importer_import_id_hash varchar(55) DEFAULT '' NOT NULL,
-	pm_importer_import_id varchar(255) DEFAULT '' NOT NULL
+	pm_importer_import_id varchar(255) DEFAULT '' NOT NULL,
+
+	KEY importhash (pm_importer_import_id_hash ,sys_language_uid, pid)
 );
 
 #
@@ -43,7 +47,9 @@ CREATE TABLE sys_category (
 #
 CREATE TABLE tx_pxaproductmanager_domain_model_attribute (
 	pm_importer_import_id_hash varchar(55) DEFAULT '' NOT NULL,
-	pm_importer_import_id varchar(255) DEFAULT '' NOT NULL
+	pm_importer_import_id varchar(255) DEFAULT '' NOT NULL,
+
+	KEY importhash (pm_importer_import_id_hash ,sys_language_uid, pid)
 );
 
 #
@@ -51,5 +57,7 @@ CREATE TABLE tx_pxaproductmanager_domain_model_attribute (
 #
 CREATE TABLE tx_pxaproductmanager_domain_model_option (
 	pm_importer_import_id_hash varchar(55) DEFAULT '' NOT NULL,
-	pm_importer_import_id varchar(255) DEFAULT '' NOT NULL
+	pm_importer_import_id varchar(255) DEFAULT '' NOT NULL,
+
+	KEY importhash (pm_importer_import_id_hash ,sys_language_uid, pid)
 );
