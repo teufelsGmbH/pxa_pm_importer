f30e5893afb750ab5d0a3df1f2b06a18d9b7a68c
anjeylink <andriy.oprysko@resultify.se>
2019-01-31 14:09:53 +0200

    [TASK] js code refactor

--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -3,6 +3,6 @@
 return [
     'pxapmimporter-progress-bar' => [
         'path' => '/pxapmimporter/progress-bar',
-        'target' => \Pixelant\PxaPmImporter\Controller\AjaxController::class . '::importProgressStatusAction'
+        'target' => \Pixelant\PxaPmImporter\Controller\ProgressBarAjaxController::class . '::importProgressStatusAction'
     ]
 ];
--- Resources/Public/JavaScript/Backend/ImportModule.js
+++ Resources/Public/JavaScript/Backend/ImportModule.js
@@ -13,87 +13,87 @@ define(['jquery'], function ($) {
 		}
 	}
 
-	let proto = ImportModule.prototype;
+	ImportModule.prototype = {
+		/**
+		 * Init main method
+		 */
+		init: function () {
+			this.submitButtonInit();
+		},
 
-	/**
-	 * Init main method
-	 */
-	proto.init = function () {
-		this.submitButtonInit();
-	};
+		/**
+		 * Submit state for import button
+		 */
+		submitButtonInit: function () {
+			this.getjQueryInstance('importSubmit').on('click', e => {
+				let $button = $(e.currentTarget);
+				$button.button('loading');
 
-	/**
-	 * Submit state for import button
-	 */
-	proto.submitButtonInit = function () {
-		this.getjQueryInstance('importSubmit').on('click', e => {
-			let $button = $(e.currentTarget);
-			$button.button('loading');
+				let parentWrapper = $button.closest(this.selectors['parentWrapper']);
+				parentWrapper.addClass('loading');
 
-			let parentWrapper = $button.closest(this.selectors['parentWrapper']);
-			parentWrapper.addClass('loading');
-
-			// Progress bar
-			let importId = parseInt(this.getjQueryInstance('selectImportBox').val());
-			this.initProgressBar(importId);
-		});
-	};
-
-	/**
-	 * Init progress bar
-	 *
-	 * @param importId
-	 */
-	proto.initProgressBar = function (importId) {
-		let progressBarTemplate = $(this.getProgressBar());
-		let progressBar = progressBarTemplate.find('.progress-bar');
-		progressBar
-			.css('width', '0%')
-			.text('0%');
-
-		this.getjQueryInstance('progressBarPlaceholder').html(progressBarTemplate);
-
-		let progress = setInterval(function () {
-			$.ajax({
-				type: 'POST',
-				url: TYPO3.settings.ajaxUrls['pxapmimporter-progress-bar'],
-				data: {
-					importId: importId,
-				}
-			}).done(({progress}) => {
-				progressBar
-					.css('width', progress + '%')
-					.text(progress + '%');
-
-				if (progressBar >= 100) {
-					clearInterval(progress);
-				}
+				// Progress bar
+				let importId = parseInt(this.getjQueryInstance('selectImportBox').val());
+				this.initProgressBar(importId);
 			});
-		}, 1000);
+		},
+
+		/**
+		 * Init progress bar
+		 *
+		 * @param importId
+		 */
+		initProgressBar: function (importId) {
+			let progressBarTemplate = $(this.getProgressBar());
+			let progressBar = progressBarTemplate.find('.progress-bar');
+			progressBar
+				.css('width', '0%')
+				.text('0%');
+
+			this.getjQueryInstance('progressBarPlaceholder').html(progressBarTemplate);
+
+			let progress = setInterval(function () {
+				$.ajax({
+					type: 'POST',
+					url: TYPO3.settings.ajaxUrls['pxapmimporter-progress-bar'],
+					data: {
+						importId: importId,
+					}
+				}).done(({progress}) => {
+					progressBar
+						.css('width', progress + '%')
+						.text(progress + '%');
+
+					if (progressBar >= 100) {
+						clearInterval(progress);
+					}
+				});
+			}, 1000);
+		},
+
+		/**
+		 * Selector for element
+		 *
+		 * @param element
+		 * @return string
+		 */
+		getjQueryInstance: function (element) {
+			return $(this.selectors[element] || '');
+		},
+
+		/**
+		 * Progress bar template
+		 *
+		 * @return {string}
+		 */
+		getProgressBar: function () {
+			return '<div class="progress">\n' +
+				'    <div class="progress-bar progress-bar-striped active" role="progressbar"\n' +
+				'         aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">\n' +
+				'    </div>\n' +
+				'</div>';
+		}
 	};
 
-	/**
-	 * Selector for element
-	 *
-	 * @param element
-	 * @return string
-	 */
-	proto.getjQueryInstance = function (element) {
-		return $(this.selectors[element] || '');
-	};
-
-	/**
-	 * Progress bar template
-	 *
-	 * @return {string}
-	 */
-	proto.getProgressBar = function () {
-		return '<div class="progress">\n' +
-			'    <div class="progress-bar progress-bar-striped active" role="progressbar"\n' +
-			'         aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">\n' +
-			'    </div>\n' +
-			'</div>';
-	};
-
-	return (new ImportModule());
+	return ImportModule;
 });
