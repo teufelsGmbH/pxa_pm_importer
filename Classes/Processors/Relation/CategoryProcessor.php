350d3b7e7712c114366a78d3122660110525946e
Andriy Oprysko <pyvasyk@gmail.com>
2020-02-13 11:24:48 +0200

    [TASK] be more explicit about 3-d argument passed

--- Classes/Processors/Relation/CategoryProcessor.php
+++ Classes/Processors/Relation/CategoryProcessor.php
@@ -17,8 +17,14 @@ class CategoryProcessor extends AbstractRelationFieldProcessor implements AbleCr
     public function createMissingEntity(string $importId)
     {
         $fields = ['title' => $importId, $this->tcaHiddenField() => 1];
+        $sysLanguageUid = 0;
 
-        $this->repository->createEmpty($importId, 'sys_category', 0, $this->newRecordFieldsWithPlaceHolder($fields));
+        $this->repository->createEmpty(
+            $importId,
+            'sys_category',
+            $sysLanguageUid,
+            $this->newRecordFieldsWithPlaceHolder($fields)
+        );
     }
 
     /**
