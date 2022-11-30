3f9f89d9439b2faa6f543e6690d46c2c8e0df46d
bh-teufels <38100744+bh-teufels@users.noreply.github.com>
2022-11-30 15:17:26 +0100

    Update ext_emconf.php
    update for Typo3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '2.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.5.99',
-            'pxa_product_manager' => '9.5.1-9.99.99'
+            'typo3' => '9.5.0-11.5.99',
+            'pxa_product_manager' => '9.5.1-0.0.0'
         ],
         'conflicts' => [],
         'suggests' => []
